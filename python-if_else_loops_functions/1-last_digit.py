250ef80a8618b05e55ea74e031489781d5c979d2
JeremyLrs <hello@laurensjeremy.com>
2025-09-03 18:26:07 +0200

    Create script that print a random number followed by the last digit of number

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+last_digit = number % 10
+text = "last digit of"
+
+if number < 0 and last_digit != 0:
+    last_digit -= 10
+
+if last_digit > 5:
+    print(f"{text} {number} is {last_digit} and is greater than 5")
+
+elif last_digit == 0:
+    print(f"{text} {number} is 0 and is 0")
+
+else:
+    print(f"{text} {number} is {last_digit} and is less than 6 and not 0")
