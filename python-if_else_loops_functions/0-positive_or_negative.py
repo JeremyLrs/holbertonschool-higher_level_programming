b57b8316d8eebf148fb356fef155ca0acdb24fa0
JeremyLrs <hello@laurensjeremy.com>
2025-09-04 16:55:24 +0200

    Change I to i in is negative

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10, 10)
 
 if number > 0:
@@ -9,4 +10,4 @@ elif number == 0:
     print(f"{number} is zero")
 
 elif number < 0:
-    print(f"{number} Is negative")
+    print(f"{number} is negative")
