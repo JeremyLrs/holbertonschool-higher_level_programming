469ac1b4aadd4b66e7420f6f6acd7a9bbcbc60b1
JeremyLrs <hello@laurensjeremy.com>
2025-09-04 16:52:55 +0200

    Fix last to last

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 
 last_digit = number % 10
-text = "last digit of"
+text = "Last digit of"
 
 if number < 0 and last_digit != 0:
     last_digit -= 10
