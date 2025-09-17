9445dca39d1a24612cdd2b945418a976aa4da1b1
JeremyLrs <hello@laurensjeremy.com>
2025-09-17 13:44:26 +0200

    Create script do define a square class with private instance attribute : size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+It's a script do define a square class
+with :  Private instance attribute: size
+"""
+
+
+class Square:
+    def __init__(self, size):
+        self.size = size
+        pass
