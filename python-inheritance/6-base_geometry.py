901b85ad08b60bad56e36ebfb7c40cd6a4ba69b7
JeremyLrs <hello@laurensjeremy.com>
2025-09-22 15:26:06 +0200

    Add instance method: def area(self): that raises an Exception with the message area() is not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+5-base_geometry.py
+Create an empty class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """
+    Empty class BaseGeometry
+    """
+    def area(self):
+        """
+        area is not implemented
+        """
+        raise Exception("area() is not implemented")
