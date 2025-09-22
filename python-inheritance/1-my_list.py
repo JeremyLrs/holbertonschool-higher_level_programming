b6557925d4d5cc53923b1383d3c51b054b1b33c8
JeremyLrs <hello@laurensjeremy.com>
2025-09-22 14:34:39 +0200

    Create script that Write a class MyList that inherits from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+1-my_list.py
+Write a class MyList that inherits from list
+Return: subclass
+"""
+
+class MyList(list):
+    def print_sorted(self):
+        print(sorted(self))
