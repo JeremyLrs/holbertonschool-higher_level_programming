956760b80331d5c24c75703520b59369e3af2bd1
JeremyLrs <hello@laurensjeremy.com>
2025-09-24 09:41:48 +0200

    Ad comment

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,6 +11,12 @@ Return: True if it is otherwise False
 
 
 def inherits_from(obj, a_class):
+    """
+    inherits_from - Check if obj inherits from a_class
+    @obj: An object
+    @a_class: A class
+    Return: True if obj inherits from a_class, else False
+    """
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
