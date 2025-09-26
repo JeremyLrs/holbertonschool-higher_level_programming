8ec197d1cde05766b24027d143688e43cdc959d0
JeremyLrs <hello@laurensjeremy.com>
2025-09-26 15:09:52 +0200

    Del condition

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,11 +2,9 @@
 
 """
 4-inherits_from.py
-Write a function that returns True if
-the object is an instance of a class
-that inherited (directly or indirectly)
-from the specified class ; otherwise False
-Return: True if it is otherwise False
+Check if the object is an instance of a class
+that inherited from the specified class
+Return: True if it is, else False
 """
 
 
@@ -17,7 +15,4 @@ def inherits_from(obj, a_class):
     @a_class: A class
     Return: True if obj inherits from a_class, else False
     """
-    if issubclass(type(obj), a_class) and type(obj) is not a_class:
-        return True
-    else:
-        return False
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
