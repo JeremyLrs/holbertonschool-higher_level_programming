eb64c19edcaf1fb06d29aa9170ac75174b14ad4d
JeremyLrs <hello@laurensjeremy.com>
2025-09-29 13:55:40 +0200

    Create script that writea string and return number of char

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''
+0-read_file.py
+Function to writes a string to a text file.
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    Function to write a string and 
+    return the number of char written
+    '''
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        write_data = f.write(text)
+
+        return write_data
