a1e8a41b451c2c0277364b90522e951b318cc6c9
JeremyLrs <hello@laurensjeremy.com>
2025-10-09 15:13:48 +0200

    change form of message

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -24,37 +24,30 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             message = "Hello, this is a simple API!"
-            self.wfile.write(message.encode("utf-8"))
 
         elif self.path == "/data":
-            data = {"name": "John", "age": 30, "city": "New York"}
-            json_data = json.dumps(data)
             self.send_response(200)
-            self.header("Content-type", "application/json")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(json_data.encode("utf-8"))
-
-        elif self.path == "/info":
-            data = {
-                "version": "1.0",
-                "description": "A simple API built with http.server"}
-            json_data = json.dumps(data)
-            self.send_response(200)
-            self.header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json_data.encode("utf-8"))
+            message = json.dumps({
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+                })
 
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"OK")
+            message = "OK"
 
         else:
-            self.send_response(404)
+            self.send_response(404, message="Not found")
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            message = "Endpoint not found"
+
+        self.wfile.write(bytes(message, "utf8"))
 
 
 def main():
