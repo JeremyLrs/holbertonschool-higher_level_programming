8bcb4abe9293e808f45064bc1065398cbc1a46f1
JeremyLrs <hello@laurensjeremy.com>
2025-10-24 15:32:29 +0200

    Create database dump of hbtn_0d_tvshows

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- List all tv shows with one genre linked
+SELECT title, genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title ASC, genre_id ASC
