49de3557619d4753641c311af7ea00eb8a788cf6
JeremyLrs <hello@laurensjeremy.com>
2025-10-24 15:38:29 +0200

    Create script that uses the hbtn_0d_tvshows database to lists all genres of the show

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- List all desxter genres
+SELECT name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter" AND tv_genres.id IS NOT NULL
+ORDER BY NAME
