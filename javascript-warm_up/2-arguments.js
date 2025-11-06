44a32377fb5699ac29730ea03339bd800aaa9bf7
JeremyLrs <hello@laurensjeremy.com>
2025-11-06 15:09:27 +0100

    Add require with node:process

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const args = process.argv.length;
 
-if (args === 2) {
-    console.log('No argument');
-} else if (args === 3) {
-    console.log('Argument found');
+const { argv } = require('node:process');
+
+if (argv.length < 3) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found')
+  console.log('Arguments found');
 }
