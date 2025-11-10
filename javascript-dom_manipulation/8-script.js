45eddee19fb0cf2d5e91cd4300f5afd7f1f51333
JeremyLrs <hello@laurensjeremy.com>
2025-11-10 16:02:57 +0100

    Create script that fetches from website and displays the value of hello from that fetch in the HTML element with id hello

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+
+fetch(url).then((response) => {
+  return response.json();
+}).then((data) => {
+  document.querySelector('#hello').textContent = data.hello;
+});
